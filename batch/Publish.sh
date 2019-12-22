a18c53d755978fa874fc9bfe2129d509c3182ee3
hiroki19990625 <hiroki19990625@gmail.com>
2019-12-23 01:39:49 +0900

    circle-ciのビルドテスト

--- batch/Publish.sh
+++ batch/Publish.sh
@@ -29,14 +29,14 @@ cd repo
 
 mkdir "app/katana-gui/windows/$git_hash" -p
 zip "katana-gui-$git_hash" ../build/launch4j/katana-gui.exe
-cp "katana-gui-$git_hash.zip" "app/katana-gui/windows/$git_hash/katana-gui.exe"
+cp "katana-gui-$git_hash.zip" "app/katana-gui/windows/$git_hash/katana-gui-$git_hash.zip"
 mkdir "app/katana-gui/windows/last_version" -p
-cp "katana-gui-$git_hash.zip" "app/katana-gui/windows/last_version/katana-gui.exe"
+cp "katana-gui-$git_hash.zip" "app/katana-gui/windows/last_version/katana-gui-$git_hash.zip"
 mkdir "app/katana-gui/jar/$git_hash" -p
 cp ../build/libs/katana-gui-1.0-SNAPSHOT-all.jar "app/katana-gui/jar/$git_hash/katana-gui-1.0-SNAPSHOT.jar"
 mkdir "app/katana-gui/jar/last_version" -p
 cp ../build/libs/katana-gui-1.0-SNAPSHOT-all.jar "app/katana-gui/jar/last_version/katana-gui-1.0-SNAPSHOT.jar"
-
+rm "katana-gui-$git_hash.zip"
 
 git add .
 git commit -m "publish repo"
